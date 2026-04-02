5a82e1cbe7418af2a157c86eb74a2fc10c43ea24
Yusuf Balogun <yuslabibalogun2705@gmail.com>
2026-04-02 11:43:44 +0100

    backend implementation and i use supabase for the datab

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,4 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl  = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey  = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+console.log("URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
 export const supabase = createClient(supabaseUrl, supabaseKey);
